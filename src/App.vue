8896492d16fc60381599f4fad722e09f8eeecf15
Mélo <melodiekouy@gmail.com>
2024-02-05 17:59:05 +0100

    add mangaCard template

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
   Hello world
+  <div>
+    <MangaCard/>
+  </div>
+  <!-- <div v-for="manga in resp" >
+  <p>{{ manga.attributes.title }}</p>
+  </div> -->
 </template>
 
 <script>
-  export default {}
+import MangaCard from '@/MangaCard.vue'
+// TOOD deplacer MangaCard.vue dans un dossier components
+export default {
+  name: 'MangasGallery',
+  components:{
+    MangaCard
+  }
+}
 </script>
---
+++ src/MangaCard.vue
@@ -0,0 +1,24 @@
+<template>
+<!-- type ? -->
+<!-- attributes -->
+    <div class="manga-card">
+        <h2>title</h2>
+        <!-- altTitles ? -->
+        <p>description</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'MangaCard'
+    }
+</script>
+
+<style scoped>
+.manga-card {
+    /* display: flex; */
+}
+h2 {
+    font-size: 20px;
+}
+</style>
