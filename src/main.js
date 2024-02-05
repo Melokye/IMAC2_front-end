dee06941798fc075213ee4f8f480976e07abda34
Mélo <melodiekouy@gmail.com>
2024-02-05 17:34:13 +0100

    test GET sur l'API

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import './assets/main.css'
+import axios, {isCancel, AxiosError} from 'axios';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const myApp = createApp(App).mount('#app')
+
+const title = 'Spy x Family';
+// const axios = require('axios');
+
+const baseUrl = 'https://api.mangadex.org';
+
+const resp = await axios({
+    method: 'GET',
+    url: `${baseUrl}/manga`,
+    params: {
+        title: title
+    }
+});
+
+console.log(resp.data.data.map(manga => manga.attributes.title));
+
+
+getMangasData(){
+    
+}
