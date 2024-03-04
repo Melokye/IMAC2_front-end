eb73c3b16b04d273812ea79db23cb783ae15ee82
Mélo <melodiekouy@gmail.com>
2024-03-04 18:08:55 +0100

    mangaAPI (on going in the main)

---
+++ services/api/mangaAPI.js
@@ -0,0 +1,20 @@
+// import axios, {isCancel, AxiosError} from 'axios';
+
+// const title = 'Spy x Family'; // TODO test
+// const baseUrl = 'https://api.mangadex.org';
+
+// const getMangasData = async function(){
+//     const response = await axios({
+//         method: 'GET',
+//         url: `${baseUrl}/manga`,
+//         params: {
+//             title: title // TODO à moduler
+//         }
+//     });
+
+//     // TODO test response.status ?
+//     // console.log(); 
+//     return resp.data.data.map(manga => manga.attributes.title); // TODO test
+// }
+
+// export {getMangasData}
