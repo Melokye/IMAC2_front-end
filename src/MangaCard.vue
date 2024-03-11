21737e3b0bb0dad8e95847dd43f697458ce14cd8
Mélo <melodiekouy@gmail.com>
2024-03-11 16:41:03 +0100

    🤔 Affichage titre

--- src/MangaCard.vue
+++ src/MangaCard.vue
@@ -2,23 +2,27 @@
 <!-- type ? -->
 <!-- attributes -->
     <div class="manga-card">
-        <h2>Title</h2>
+        <h2>Title : {{title}}</h2>
         <p>Drapeau d'origine ?</p>
         <p>IMG</p>
         altTitles ?
         <p>Author</p>
         <p>Tag ?</p>
 
-        
         <p>Status : nb de chapitres pas encore lu</p>
         <p>Description rapide</p>
         <p>Lien vers le dernier chapitre (lu ?)</p>
+        <p>Obligatoire : scanlation groups</p>
     </div>
 </template>
 
 <script>
     export default{
-        name: 'MangaCard'
+        name: 'MangaCard',
+        props: {
+            title: String,
+            // description: String
+        }
     }
 </script>
 
