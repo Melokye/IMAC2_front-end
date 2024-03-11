f2fc01d086d6d4cc1d4d05aa1d116383c8fc5a5e
Mélo <melodiekouy@gmail.com>
2024-03-11 16:39:53 +0100

    appel API -> founction

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './assets/main.css'
-import axios, {isCancel, AxiosError} from 'axios';
+import axios, {isCancel, AxiosError} from 'axios'; // TODO test
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -7,18 +7,23 @@ import App from './App.vue'
 const myApp = createApp(App)
 myApp.mount('#app')
 
-// --- Test
-const title = 'Spy x Family';
-
+// --- Test 
+const title = 'Spy x Family'; // TODO test
 const baseUrl = 'https://api.mangadex.org';
 
-const resp = await axios({
-    // TODO async
-    method: 'GET',
-    url: `${baseUrl}/manga`,
-    params: {
-        title: title
-    }
-});
+const getMangasData = async function(){
+    const response = await axios({
+        method: 'GET',
+        url: `${baseUrl}/manga`,
+        params: {
+            title: title // TODO à moduler
+        }
+    });
+    
+    console.log("called"); // TODO 
+    // TODO test response.status ?
+    return response.data.data.map(manga => manga.attributes); // TODO test
+}
 
-console.log(resp.data.data.map(manga => manga.attributes.title));
+// console.log(getMangasData()); // TODO test
+export {getMangasData}
