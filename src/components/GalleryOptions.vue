00161974e83d416911261abdc42494278221021e
Mélodie KOUY <melodiekouy@gmail.com>
2024-04-16 03:44:50 +0200

    supp implementation non utilisé

--- src/components/GalleryOptions.vue
+++ src/components/GalleryOptions.vue
@@ -1,47 +1,28 @@
 <template>
-  <!-- <div> TODO class="gallery-options" -->
+  <!-- TODO <div> class="gallery-options" -->
     <!-- TODO v-on:change="retrieveMangasData()" -->
-    <!-- <input 
+    <input 
       type="text"
       :value="search"
       @change="onSearchChanged" 
       name="search" 
       placeholder="Chercher un manga">
-    <label for="manga-sort">Trier par : </label> -->
-
-    <select 
-      :value="mangasSortType"
-      @input="onMangasSortTypeChanged"
-    >
-
-      <!-- TODO a mettre dans un nouveau fichier ? -->
-      <!-- TODO mettre les options dans un dico https://fr.vuejs.org/v2/guide/forms.html#Select -->
-      <option value="AZName">Noms de A à Z</option>
-      <option value="ZAName">Noms de Z à A</option>
-    </select>
+    <label for="manga-sort">Trier par : </label>
   <!-- </div> -->
-  
-  
 </template>
 
 <script>
   export default {
     name: 'GalleryOptions',
-    emit: ["update:search", "update:mangasSortType"],
+    emit: ["update:search"],
     watch: {
       search: function(newSearch) {
         localStorage.setItem("search", newSearch);
       },
-      mangasSortType: function(newMangasSortType) {
-        localStorage.setItem("mangasSortType", newMangasSortType);
-      }
     },
     methods: {
       onSearchChanged(event) {
         this.$emit('update:search', event.target.value);
-      },
-      onMangasSortTypeChanged(event) {
-        this.$emit('update:mangasSortType', event.target.value);
       },
     }
   }
