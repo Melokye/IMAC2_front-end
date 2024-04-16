52b712b41310d9fe099e88cb66139ec133d2a1c7
Mélodie KOUY <melodiekouy@gmail.com>
2024-04-16 03:47:23 +0200

    ajout attributs

--- src/services/api/mangaAPI.js
+++ src/services/api/mangaAPI.js
@@ -1,21 +1,21 @@
 import axios, {isCancel, AxiosError} from 'axios';
 export {getMangasData}
 
-// const title = 'Spy x Family'; // TODO test
-const baseUrl = 'https://api.mangadex.org';
-
 const getMangasData = async function(title){
     const response = await axios({
         method: 'GET',
-        url: `${baseUrl}/manga`,
+        url: `https://api.mangadex.org/manga/`,
         params: {
-            title: title // TODO à moduler?
-        }
+            title: title, // TODO à moduler?,
+            includes: ["manga", "cover_art", "author", "artist", "tag", "creator"]
+        },
+        
     });
     
     if(response.status != 200){
         console.log("error");
         // TODO voir diapo
     }
+    console.log(response.data.data); // TODO debug
     return response.data.data; // .map(manga => manga.attributes)
 }
